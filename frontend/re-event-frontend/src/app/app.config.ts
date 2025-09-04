e95f25c8decd10e6419a6174583c8951cb7d7bae
Juan Jose Miranda <jjsmm97@gmail.com>
2025-09-03 22:04:37 -0400

    feat(frontend): configure build process and update environment settings for production deployment

--- frontend/re-event-frontend/src/app/app.config.ts
+++ frontend/re-event-frontend/src/app/app.config.ts
@@ -23,7 +23,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     provideAnimationsAsync(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
